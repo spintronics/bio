c7ecb411a1c68bdd3490da19760ae4afd999ca75
Kevin Brown <kevinbrown5124@gmail.com>
2022-09-06 11:47:06 -0500

    added html only page and theme toggle

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
 function main() {
-  // let toggleStylesButton = document.getElementById("toggle-styles")
-  // let stylesEnabled = true;
-  // let page = document.getElementsByTagName("html")[0];
+  let toggleTheme = document.getElementById("toggle-theme");
+  let theme = 0;
+  let root = document.documentElement;
 
-  // toggleStylesButton.addEventListener("click", () => {
-  //   page.className = stylesEnabled ? 'styles-disabled' : '';
-  //   stylesEnabled = !stylesEnabled;
-  // })
+
+
+  toggleTheme.addEventListener("click", () => {
+    theme = !theme;
+    if (theme) {
+      // light theme
+      root.style.setProperty("--header-background", "#fff");
+      root.style.setProperty("--header-text", "#000");
+      root.style.setProperty("--background1", "#F7F3F5");
+      root.style.setProperty("--background2", "#EFF7FF");
+      root.style.setProperty("--text", "#000");
+
+      toggleTheme.querySelector("img").src = "/assets/icons/sunny.png";
+    } else {
+      // dark theme
+      root.style.setProperty('--header-background', '#161b22');
+      root.style.setProperty('--header-text', '#f0f6fc');
+      root.style.setProperty('--background1', '#0d1117');
+      root.style.setProperty('--text', '#c9d1d9');
+      root.style.setProperty('--background2', '#010409');
+      toggleTheme.querySelector("img").src = "/assets/icons/half-moon.png";
+    }
+  })
 }
 document.addEventListener("DOMContentLoaded", main);
