176e2cd78733653ae260d48ce8232d70457131f7
Kevin Brown <kevinbrown5124@gmail.com>
2022-09-06 09:15:22 -0500

    :seedling:

---
+++ script.js
@@ -0,0 +1,11 @@
+function main() {
+  // let toggleStylesButton = document.getElementById("toggle-styles")
+  // let stylesEnabled = true;
+  // let page = document.getElementsByTagName("html")[0];
+
+  // toggleStylesButton.addEventListener("click", () => {
+  //   page.className = stylesEnabled ? 'styles-disabled' : '';
+  //   stylesEnabled = !stylesEnabled;
+  // })
+}
+document.addEventListener("DOMContentLoaded", main);
