d58e61863b5ec420ca0f4a97d688d2dc05b4c8d3
Kevin Brown <kevinbrown5124@gmail.com>
2022-09-11 13:52:08 -0500

    updated qualifications/service pages to match template, added whitespace to code, changed bio text color, small style changes

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function main() {
-  let toggleTheme = document.getElementById("toggle-theme");
-  let theme_style = document.getElementById("theme_style");
-  let theme = theme_style.getAttribute("href").includes("light");
+  let stylesheetElement = document.getElementById("theme_style");
+  let theme = stylesheetElement.getAttribute("href").includes("light");
 
 
 
   toggleTheme.addEventListener("click", () => {
+
     theme = !theme;
-    debugger;
+
     if (theme) {
       // light theme
       // switch stylesheet
-      theme_style.href = theme_style.href.replace("dark", "light");
+      stylesheetElement.href = stylesheetElement.href.replace("dark", "light");
 
       // switch icon
       toggleTheme.querySelector("img").src = toggleTheme.querySelector("img").src.replace("half-moon", "sunny");
@@ -21,6 +21,7 @@ function main() {
       if (home) {
         home.href = home.href.replace("index.html", "index-light.html");
       }
+
       // switch links
       document.querySelectorAll("[href*='dark.html']").forEach((el) => {
         el.href = el.href.replace("dark", "light");
@@ -29,16 +30,20 @@ function main() {
 
     } else {
       // dark theme
-      theme_style.href = theme_style.href.replace("light", "dark");
+      stylesheetElement.href = stylesheetElement.href.replace("light", "dark");
+
       toggleTheme.querySelector("img").src = toggleTheme.querySelector("img").src.replace("sunny", "half-moon");
+
       let home = document.querySelector(["[href*='index-light.html']"]);
       if (home) {
         home.href = home.href.replace("index-light.html", "index.html");
       }
+
       document.querySelectorAll("[href*='light.html']").forEach((el) => {
         el.href = el.href.replace("light", "dark");
       });
     }
   })
 }
+
 document.addEventListener("DOMContentLoaded", main);
